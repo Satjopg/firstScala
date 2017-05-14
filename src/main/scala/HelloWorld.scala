b620ea5137168b88f54bfc4b8b36ae12f80bfaff
Satjopg <sss.jopg@me.com>
2017-05-14 14:52:13 +0900

    edit HelloWorld

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,3 +1,14 @@
-object HelloWorld extends App {
-  println("Hello World!")
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    if(args.size == 0) {
+      val strs = Array("Hello", "World")
+      output(strs)
+    } else {
+      println("Invalid args")
+    }
+  }
+  def output(strs: Array[String]): Unit = {
+    strs.foreach(printf("%s", _))
+    println()
+  }
 }
