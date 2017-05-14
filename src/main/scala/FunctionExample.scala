d661d1be5d5ea7c751fa53c12b8c1009c962c6cc
Satjopg <sss.jopg@me.com>
2017-05-14 20:58:56 +0900

    edit Function

--- src/main/scala/FunctionExample.scala
+++ src/main/scala/FunctionExample.scala
@@ -1,7 +1,12 @@
 object FunctionExample {
-    def main(args: Array[String]): Unit = {
-      var calculate =(x: Int) => x + 5 : Int
-      println("y="+calculate(1))
-      println("y="+calculate(2))
+  def main(args: Array[String]): Unit = {
+    var calculate =(x: Int) => x + 5
+    println("y="+calculate(1))
+    println("y="+calculate(2))
+    val add = (x: Int) => {
+      val _add = (y: Int) => y + 1
+      _add(x)
     }
+    println("add result:"+add(1))
+  }
 }
